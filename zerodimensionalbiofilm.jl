ffda5a5be036b174fb69a7ab383a0cca45ecab71
alex maz <65697527+belmonts@users.noreply.github.com>
2023-12-06 14:34:24 -0500

    Update zerodimensionalbiofilm.jl
    fixed code on MarsHotel

--- zerodimensionalbiofilm.jl
+++ zerodimensionalbiofilm.jl
@@ -1,5 +1,5 @@
 #Author: belmontz, sept 5th, 2023
-module zerobioreactor
+
 #Call all the packages we will be using for our sensitivity analysis
 using Plots
 using DifferentialEquations
@@ -9,9 +9,9 @@ pyplot()
 
 #Define our function containing all the reaction parameters of our problem
 function reactionparameter_def()
-    q = 0.01
+    q = 1.2
     μ = 6.0
-    δ = 0.01
+    δ = 1.0
     α = 0.01
     Y = 2.0
     β = 1.0
@@ -21,6 +21,16 @@ function reactionparameter_def()
     return rpvc
 end
 
+function monod(c, ks)
+
+  if c <= 0.0
+    return m = 0.0
+  else
+    return m = c/(c + ks)
+  end
+
+  return m
+end
 #Define our single species CSTR model
 function simpleCSTR(du, u, p, t)
     q = p[1]
@@ -41,7 +51,8 @@ function simpleCSTR(du, u, p, t)
 
 #Globally define our timescale, initial conditions, and reaction parameters for CSTR
 #u0 = [0.0; 25.0; 0.0]
-#rpvc = reactionparameter_def()
+
+rpvc = reactionparameter_def()
 
 #Define our given problem
 #prob = ODEProblem(simpleCSTR, u0, tspan, rpvc)
@@ -58,20 +69,49 @@ function zerodim(du, u, p, t)
   ks = p[7]
   Sn = p[8]
 
-  du[1] = -q*u[1] + μ*(u[3]/(ks + u[3]))*u[1] + δ*u[2] - α*u[1]
-  du[2] = μ*(u[3]/(ks + u[3]))*u[2] - δ*u[2] + α*u[1]
-  du[3] = q*(Sn - u[3]) - (μ/Y)*(u[3]/(ks + u[3]))*(u[1] + u[2])
-  du[4] = -q*u[4] + β*(u[3]/(ks + u[3]))*(u[1] + u[2])
+  du[1] = -q*u[1] + μ*monod(u[3], ks)*u[1] - δ*u[2] + α*u[1]
+  du[2] = μ*(monod(u[3], ks))*u[2] + δ*u[2] - α*u[1]
+  du[3] = q*(Sn - u[3]) - (μ/Y)*(monod(u[3], ks))*(u[1] + u[2])
+  du[4] = -q*u[4] + β*(monod(u[3], ks))*(u[1] + u[2])
 end
 
-u0z = [0.0; 0.0; 5.0; 0.0]
 
-probz = ODEProblem(zerodim, u0z, tspan, rpvc)
 
-solz = solve(probz)
+function zerodim2D(du, u, p, t)
+  q = p[1]
+  μ = p[2]
+  δ = p[3]
+  α = p[4]
+  Y = p[5]
+  β = p[6]
+  ks = p[7]
+  Sn = p[8]
 
-plot(solz)
-
+  du[1] = -q*u[1] + μ*monod(u[3], ks)*u[1] - δ*u[2]^2 + α*u[1]^2
+  du[2] = μ*(monod(u[3], ks))*u[2] + δ*u[2]^2 - α*u[1]^2
+  du[3] = q*(Sn - u[3]) - (μ/Y)*(monod(u[3], ks))*(u[1] + u[2])
+  du[4] = -q*u[4] + β*(monod(u[3], ks))*(u[1] + u[2])
 end
+
+
+u0 = [1.0; 0.0; 5.0; 0.0]
+
+tspan = (0.0, 10.0)
+
+prob = ODEProblem(zerodim, u0, tspan, rpvc)
+
+prob2D = ODEProblem(zerodim2D, u0, tspan, rpvc)
+
+sol = solve(prob)
+
+sol2D = solve(prob2D)
+
+
+dim1plot = plot(sol)
+
+dim2plot = plot(sol2D)
+
+plot(dim1plot, dim2plot)
+
 
 
